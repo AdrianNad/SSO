5cd982af4e91814a06427ea3f3d207e5aad33f62
Aserinox <adriannad@interia.pl>
2018-12-31 14:26:45 +0100

    changed security from web to resourceServer

--- client1/src/main/java/com/sec/client1/ResourceConfig.java
+++ client1/src/main/java/com/sec/client1/ResourceConfig.java
@@ -3,11 +3,11 @@ package com.sec.client1;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableOAuth2Sso
-public class ResourceConfig extends WebSecurityConfigurerAdapter {
+public class ResourceConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
