dbe53a5b842c7be110b644b75914bf7d7b129e57
Aserinox <adriannad@interia.pl>
2019-01-03 23:29:28 +0100

    added client 2

---
+++ client2/src/main/java/com/sec/client2/ResourceConfig.java
@@ -0,0 +1,23 @@
+package com.sec.client2;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class ResourceConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/index**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
---
+++ client2/src/main/java/com/sec/client2/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.sec.client2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+
+    @GetMapping()
+    public void test() {
+        System.out.println("haha");
+    }
+}
