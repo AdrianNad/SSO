7eb17dbbeaa727e4a198e96f241f0f50090c3511
Aserinox <adriannad@interia.pl>
2019-01-14 00:29:54 +0100

    add filter which checks if token is not expired to clients

--- client1/src/main/java/com/sec/client1/ResourceConfig.java
+++ client1/src/main/java/com/sec/client1/ResourceConfig.java
@@ -1,19 +1,22 @@
 package com.sec.client1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 @Configuration
 @EnableOAuth2Sso
 public class ResourceConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private TokenExpirationFilter tokenExpirationFilter;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-
-        http
+        http.addFilterBefore(tokenExpirationFilter, AnonymousAuthenticationFilter.class)
                 .antMatcher("/**")
                 .authorizeRequests()
                 .antMatchers("/", "/index**")
---
+++ client1/src/main/java/com/sec/client1/TokenExpirationFilter.java
@@ -0,0 +1,38 @@
+package com.sec.client1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+@Component
+public class TokenExpirationFilter implements Filter {
+
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (oAuth2ClientContext != null) {
+            OAuth2AccessToken accessToken = oAuth2ClientContext.getAccessToken();
+            if (accessToken != null && accessToken.isExpired()) {
+                SecurityContextHolder.getContext().setAuthentication(null);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
