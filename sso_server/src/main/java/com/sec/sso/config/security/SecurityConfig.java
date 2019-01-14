3e0035f7953e3f6e84709932e60b1a81ab329cdd
Aserinox <adriannad@interia.pl>
2019-01-14 23:03:18 +0100

    add logout to authorization server

--- sso_server/src/main/java/com/sec/sso/config/security/SecurityConfig.java
+++ sso_server/src/main/java/com/sec/sso/config/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requestMatchers()
-                .antMatchers("/login", "/oauth/authorize")
+                .antMatchers("/login", "/oauth/authorize", "/users/logout")
                 .and()
                 .authorizeRequests()
                 .anyRequest().authenticated()
--- sso_server/src/main/java/com/sec/sso/controller/UserController.java
+++ sso_server/src/main/java/com/sec/sso/controller/UserController.java
@@ -3,8 +3,14 @@ package com.sec.sso.controller;
 import com.sec.sso.model.User;
 import com.sec.sso.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.Principal;
 
 @RestController()
@@ -22,6 +28,24 @@ public class UserController {
     public Principal user(Principal principal) {
         System.out.println(principal);
         return principal;
+    }
+
+    @RequestMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, null, null);
+        try {
+            String mainPage = getMainPage(request.getHeader("referer"));
+            response.sendRedirect(mainPage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getMainPage(String referer) throws MalformedURLException {
+        URL url = new URL(referer);
+        String protocol = url.getProtocol();
+        String authority = url.getAuthority();
+        return String.format("%s://%s", protocol, authority);
     }
 
     @PostMapping()
